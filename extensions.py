558313f9b19cdffba875bea693e4fabd7f25aa1f
Urooj Akmal <uroojakmal8@gmail.com>
2024-08-21 22:51:59 +0500

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,29 @@
+manager_extensions = {
+    # Dictionary to map keywords or descriptions to corresponding extension numbers
+
+    # Dummy Managers: These entries represent example managers and their associated extensions.
+    # You'll need to replace these with the actual manager names and extension numbers relevant to your system.
+    'dummy manager 1': ['990'],
+    'dummy manager 2': ['991'],
+    'dummy manager 3': ['992'],
+    'dummy manager 4': ['993'],
+    'dummy manager 5': ['994'],
+    'dummy manager 6': ['995'],
+    'dummy manager 7': ['996'],
+    'dummy manager 8': ['997'],
+    'dummy manager 9': ['998'],
+    'dummy manager 0': ['999'],
+    
+    # Plant Areas: These entries represent example plant areas and their associated extensions.
+    # You'll need to modify these to match your specific plant areas and extension numbers.
+    'Plant Area 0': ['660'],
+    'Plant Area 1': ['661'],
+    'Plant Area 2': ['662'],
+    'Plant Area 3': ['663'],
+    'Plant Area 4': ['664'],
+    'Plant Area 5': ['665'],
+    'Plant Area 6': ['666'],
+    'Plant Area 7': ['667'],
+    'Plant Area 8': ['668'],
+    'Plant Area 9': ['669']
+}
