dc4acd9a2d0e6da23b15f5c8c6a3afba87e9c903
Urooj Akmal <uroojakmal8@gmail.com>
2024-08-22 20:32:57 +0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -2,7 +2,7 @@ responses = {
     # Greetings and Casual Conversation: These responses are used for basic greetings and casual interactions with the user.
     "hello": "Hi there! How can I assist you today?",
     "hi": "Hello! What can I do for you?",
-    "who are you": "I'm your Onboarding Buddy here to help you with information about Lucky Core Industries Soda Ash Buisness and its various facilities. If you have any questions or need assistance, feel free to ask!",
+    "who are you": "I'm your Onboarding Buddy here to help you with information about the Buisness and its various facilities. If you have any questions or need assistance, feel free to ask!",
     "how are you": "I'm just a bot, but I'm here to help! How can I assist you today?",
     "what's up": "Not much, just here to help you out! How can I assist you today?",
     "bye": "Goodbye! Have a fantastic day!",
@@ -13,11 +13,4 @@ responses = {
     "can you tell me a joke": "Why don't robots get lost? Because they always follow the right algorithm!",
     "who is your mother": "This chatbot was designed by Urooj Akmal. She aimed to create a helpful tool to assist with onboarding and company information. Hope you find it useful!"
     # Add more responses as needed:
-    # add responses for specific questions about Lucky Core Industries Soda Ash Business, its facilities, or any other relevant topics.
-    # For example:
-    # "what is soda ash?": "Soda ash, also known as sodium carbonate, is a chemical compound...",
-    # "where are Lucky Core's soda ash business located?": "Lucky Core has soda ash business in...",
-}
-
-
-
+    # add responses for specific questions about the Business, its facilities, or any other relevant topics.
